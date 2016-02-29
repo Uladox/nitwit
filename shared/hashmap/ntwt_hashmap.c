e90e4079f273f1e6fdf6df80132ddffb2601e482
Uladox <uladoxiental@gmail.com>
2016-02-28 16:06:49 -1000

    Changed to meet linux coding style.

--- shared/hashmap/ntwt_hashmap.c
+++ shared/hashmap/ntwt_hashmap.c
@@ -1,9 +1,9 @@
 /*    This file is part of nitwit.
  *
  *    nitwit is free software: you can redistribute it and/or modify
- *    it under the terms of the Lesser GNU General Public License as published by
- *    the Free Software Foundation, either version 3 of the License, or
- *    (at your option) any later version.
+ *    it under the terms of the Lesser GNU General Public License as
+ *    published by the Free Software Foundation, either version 3 of
+ *    the License, or (at your option) any later version.
  *
  *    nitwit is distributed in the hope that it will be useful,
  *    but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -11,7 +11,7 @@
  *    Lesser GNU General Public License for more details.
  *
  *    You should have received a copy of the Lesser GNU General Public License
- *    along with Ruspma.  If not, see <http://www.gnu.org/licenses/>.
+ *    along with nitwit.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include "ntwt_hashmap.h"
@@ -22,18 +22,18 @@
 #define BIN_MAX_DENSITY 5
 #define HASH_SEED 37
 
-const static int hashmap_primes[] = {
+static const int hashmap_primes[] = {
 	8 + 3, 16 + 3, 32 + 5, 64 + 3, 128 + 3, 256 + 27, 512 + 9,
 	1024 + 9, 2048 + 5, 4096 + 3, 8192 + 27, 16384 + 43, 32768 + 3,
 	65536 + 45, 131072 + 29, 262144 + 3, 524288 + 21, 1048576 + 7,
-        2097152 + 17, 4194304 + 15, 8388608 + 9, 16777216 + 43, 33554432 + 35,
+	2097152 + 17, 4194304 + 15, 8388608 + 9, 16777216 + 43, 33554432 + 35,
 	67108864 + 15, 134217728 + 29, 268435456 + 3, 536870912 + 11,
 	1073741824 + 85, 0
 };
 
 static void rehash(struct ntwt_hashmap *map);
 
-#define ROT32(x, y) ((x << y) | (x >> (32 - y))) // avoid effort
+#define ROT32(x, y) ((x << y) | (x >> (32 - y)))
 uint32_t murmur3_32(const char *key, uint32_t len, uint32_t seed)
 {
 	static const uint32_t c1 = 0xcc9e2d51;
@@ -49,6 +49,7 @@ uint32_t murmur3_32(const char *key, uint32_t len, uint32_t seed)
 	const uint32_t *blocks = (const uint32_t *) key;
 	int i;
 	uint32_t k;
+
 	for (i = 0; i < nblocks; i++) {
 		k = blocks[i];
 		k *= c1;
@@ -86,32 +87,25 @@ uint32_t murmur3_32(const char *key, uint32_t len, uint32_t seed)
 	return hash;
 }
 
-/* Copies value of request pointer into the hashbin */
 static struct ntwt_hashentry *new_hashentry(void *key, uint32_t key_size,
 					    void *storage)
 {
 	struct ntwt_hashentry *entry = malloc(sizeof(*entry));
 
-        entry->key = key;
+	entry->key = key;
 	entry->key_size = key_size;
-        entry->storage = storage;
-        entry->next = NULL;
+	entry->storage = storage;
+	entry->next = NULL;
 	return entry;
 }
 
-/* frees hashbin without freeing request, used in rehashing */
-static void hashentry_free(struct ntwt_hashentry *hashentry)
-{
-	free(hashentry);
-}
-
-/* creates a new hashmap */
 struct ntwt_hashmap *ntwt_hashmap_new(unsigned int sequence,
 				      int (*compare)(void *key1,
 						     uint32_t key1_size,
 						     void *key2,
 						     uint32_t key2_size),
-				      void (*free_contents)(void *key, void *storage))
+				      void (*free_contents)(void *key,
+							    void *storage))
 {
 	struct ntwt_hashmap *map = malloc(sizeof(*map));
 	struct ntwt_hashbin *bin;
@@ -121,10 +115,11 @@ struct ntwt_hashmap *ntwt_hashmap_new(unsigned int sequence,
 	map->bin_num = hashmap_primes[sequence];
 	map->bins = malloc(sizeof(*map->bins) * hashmap_primes[sequence]);
 	map->entry_num = 0;
-        map->primes_pointer = hashmap_primes;
+	map->primes_pointer = hashmap_primes;
 	bin = map->bins;
 
 	int i;
+
 	for (i = 0; i != hashmap_primes[sequence]; ++i) {
 		bin->first = NULL;
 		++bin;
@@ -132,12 +127,12 @@ struct ntwt_hashmap *ntwt_hashmap_new(unsigned int sequence,
 	return map;
 }
 
-/* Final freeing of hashmap */
 void ntwt_hashmap_free(struct ntwt_hashmap *map)
 {
 	struct ntwt_hashbin *bin = map->bins;
 
 	int i;
+
 	for (i = 0; i != map->bin_num; ++i, ++bin) {
 		struct ntwt_hashentry *entry = bin->first;
 
@@ -145,7 +140,7 @@ void ntwt_hashmap_free(struct ntwt_hashmap *map)
 			struct ntwt_hashentry *tmp = entry;
 
 			map->free_contents(entry->key, entry->storage);
-		        entry = entry->next;
+			entry = entry->next;
 			free(tmp);
 		}
 	}
@@ -153,7 +148,6 @@ void ntwt_hashmap_free(struct ntwt_hashmap *map)
 	free(map);
 }
 
-/* Adds an element to the hashmap */
 int ntwt_hashmap_add(struct ntwt_hashmap *map,
 		     void *key, uint32_t key_size,
 		     void *storage)
@@ -165,7 +159,7 @@ int ntwt_hashmap_add(struct ntwt_hashmap *map,
 		rehash(map);
 
 	row = murmur3_32(key, key_size, HASH_SEED) % map->bin_num;
-        entry = map->bins[row].first;
+	entry = map->bins[row].first;
 
 	if (!entry) {
 		map->bins[row].first = new_hashentry(key, key_size, storage);
@@ -195,7 +189,7 @@ void ntwt_hashmap_remove(struct ntwt_hashmap *map, void *key,
 	unsigned int row;
 
 	row = murmur3_32(key, key_size, HASH_SEED) % map->bin_num;
-        entry = map->bins[row].first;
+	entry = map->bins[row].first;
 
 	if (!entry)
 		return;
@@ -208,8 +202,8 @@ void ntwt_hashmap_remove(struct ntwt_hashmap *map, void *key,
 	}
 
 	struct ntwt_hashentry *next;
-	for (next = entry->next; entry->next; entry = next) {
 
+	for (next = entry->next; entry->next; entry = next) {
 		if (map->compare(next->key, next->key_size, key, key_size)) {
 			entry->next = next->next;
 			map->free_contents(next->key, next->storage);
@@ -220,13 +214,12 @@ void ntwt_hashmap_remove(struct ntwt_hashmap *map, void *key,
 	}
 }
 
-/* Gets an element from a hashmap */
 void *ntwt_hashmap_get(struct ntwt_hashmap *map, void *key, uint32_t key_size)
 {
 	struct ntwt_hashentry *entry;
 	unsigned int row;
 
-        row = murmur3_32(key, key_size, HASH_SEED) % map->bin_num;
+	row = murmur3_32(key, key_size, HASH_SEED) % map->bin_num;
 
 	entry = map->bins[row].first;
 	while (entry) {
@@ -253,9 +246,9 @@ static void rehash_add(struct ntwt_hashbin *bin,
 	tmp->next = entry;
 }
 
-/* Rehashes the hashmap */
 static void rehash(struct ntwt_hashmap *map)
 {
+	struct ntwt_hashbin *bin = map->bins;
 	struct ntwt_hashbin *new_bins;
 	unsigned int new_bin_num;
 
@@ -263,13 +256,13 @@ static void rehash(struct ntwt_hashmap *map)
 	new_bins = malloc(sizeof(*new_bins) * new_bin_num);
 
 	unsigned int i;
+
 	for (i = 0; i != new_bin_num; ++i)
 		new_bins[i].first = NULL;
 
-	struct ntwt_hashbin *bin = map->bins;
 	for (i = 0; i != map->bin_num; ++i, ++bin) {
-
 		struct ntwt_hashentry *entry = bin->first;
+
 		while (entry) {
 			unsigned int row;
 			struct ntwt_hashentry *tmp = entry->next;
--- shared/hashmap/ntwt_hashmap.h
+++ shared/hashmap/ntwt_hashmap.h
@@ -1,9 +1,9 @@
 /*    This file is part of nitwit.
  *
  *    nitwit is free software: you can redistribute it and/or modify
- *    it under the terms of the Lesser GNU General Public License as published by
- *    the Free Software Foundation, either version 3 of the License, or
- *    (at your option) any later version.
+ *    it under the terms of the Lesser GNU General Public License as
+ *    published by the Free Software Foundation, either version 3 of
+ *    the License, or (at your option) any later version.
  *
  *    nitwit is distributed in the hope that it will be useful,
  *    but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -11,7 +11,7 @@
  *    Lesser GNU General Public License for more details.
  *
  *    You should have received a copy of the Lesser GNU General Public License
- *    along with Ruspma.  If not, see <http://www.gnu.org/licenses/>.
+ *    along with nitwit.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef NTWT_HASHMAP_H
